408208957e447befe94081c179811f9737a003d1
JainAdhya <jainadhya6@gmail.com>
2026-09-17 19:41:37 +0000

    Add mandatory file locking experiment

--- Hands-On-List-1-SS/16/mandatory_lock.c
+++ Hands-On-List-1-SS/16/mandatory_lock.c
@@ -3,14 +3,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main() {
     int fd;
     struct flock lock;
+    char choice;
 
-    if (argc != 2) {
-        printf("Usage: %s <r/w>\n", argv[0]);
-        return 1;
-    }
+    printf("Enter r for read lock and w for write lock: ");
+    scanf(" %c", &choice);
 
     // Open file in read-write mode
     fd = open("sample.txt", O_RDWR | O_CREAT, 0660);
@@ -27,16 +26,16 @@ int main(int argc, char *argv[]) {
     lock.l_pid = getpid();
 
     // Select read or write lock
-    if (argv[1][0] == 'w') {
+    if (choice == 'w') {
         lock.l_type = F_WRLCK;
         printf("Trying to acquire WRITE lock...\n");
     }
-    else if (argv[1][0] == 'r') {
+    else if (choice == 'r') {
         lock.l_type = F_RDLCK;
         printf("Trying to acquire READ lock...\n");
     }
     else {
-        printf("Use 'r' for read lock or 'w' for write lock.\n");
+        printf("Invalid choice. Enter r for read lock or w for write lock.\n");
         close(fd);
         return 1;
     }
@@ -56,6 +55,7 @@ int main(int argc, char *argv[]) {
     printf("Process ID: %d\n", getpid());
     printf("Lock is held. Press Enter to release the lock...\n");
 
+    getchar();
     getchar();
 
     // Unlock
@@ -77,18 +77,20 @@ int main(int argc, char *argv[]) {
 
 /* Output:
 adhya@adhya:~/System-Software-Hands-On-List-1/Hands-On-List-1-SS/16$ gcc mandatory_lock.c -o mandatory_lock
-adhya@adhya:~/System-Software-Hands-On-List-1/Hands-On-List-1-SS/16$ echo "System Software Mandatory Locking" > sample.txt
-adhya@adhya:~/System-Software-Hands-On-List-1/Hands-On-List-1-SS/16$ ./mandatory_lock w
-Trying to acquire WRITE lock...
-WRITE lock acquired.
-Process ID: 4859
+adhya@adhya:~/System-Software-Hands-On-List-1/Hands-On-List-1-SS/16$ ./manda
+tory_lock
+Enter r for read lock and w for write lock: r
+Trying to acquire READ lock...
+READ lock acquired.
+Process ID: 5058
 Lock is held. Press Enter to release the lock...
 Lock released.
 
-adhya@adhya:~/System-Software-Hands-On-List-1/Hands-On-List-1-SS/16$ ./mandatory_lock r
-Trying to acquire READ lock...
-READ lock acquired.
-Process ID: 4869
+adhya@adhya:~/System-Software-Hands-On-List-1/Hands-On-List-1-SS/16$ ./mandatory_lock
+Enter r for read lock and w for write lock: w
+Trying to acquire WRITE lock...
+WRITE lock acquired.
+Process ID: 5069
 Lock is held. Press Enter to release the lock...
 Lock released.
 */
